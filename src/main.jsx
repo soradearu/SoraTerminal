3c0bd93385893c168ec275f60e3499325d3adb2a
Sora DLL <smkaraca06@gmail.com>
2026-08-25 22:01:23 +0300

    deleted siem

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+ <React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+</React.StrictMode>
 )
